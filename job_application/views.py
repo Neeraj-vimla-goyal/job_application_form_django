d4af9ec0fb2323b884c3d1ffcadd540f8e7f9bbe
neeraj-vimla-goyal <neeraj.vimla.goyal@gmail.com>
2023-05-21 14:27:30 +0530

    Implemented the email feature

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -20,6 +21,12 @@ def index(request):
                                 date=date,
                                 occupation=occupation)
 
+            message_body = f"New Job application was submitted Thank you , {first_name}."
+            email_message = EmailMessage("Form submission confirmation", message_body, to=[email])
+            email_message.send()
+
             messages.success(request, "Form submitted successfully")
+
+
 
     return render(request, "index.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from job_application.cred import get_cred
+app_password, sender = get_cred()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = sender
+EMAIL_HOST_PASSWORD = app_password
